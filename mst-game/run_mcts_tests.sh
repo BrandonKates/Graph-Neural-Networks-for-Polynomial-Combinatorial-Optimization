0e57ecfaf9057d90af86b93562457ec00e97f26f
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-12-11 03:10:12 +0000

    more results

--- mst-game/run_mcts_tests.sh
+++ mst-game/run_mcts_tests.sh
@@ -33,3 +33,24 @@ python -u mcts.py --num_nodes 20 --num_games 1000 --max_simulations 3 --rollout_
 
 python -u mcts.py --num_nodes 30 --num_games 1000 --max_simulations 5 --rollout_count 10 --game_version=easy --game=mst --quiet > "mcts_results/easy30_5sim_10roll_node.out" 2> "mcts_results/easy30_5sim_10roll_node.err" &
 python -u mcts.py --num_nodes 30 --num_games 1000 --max_simulations 3 --rollout_count 3 --game_version=easy --game=mst --quiet > "mcts_results/easy30_3sim_3roll_node.out" 2> "mcts_results/easy30_3sim_3roll_node.err" &
+
+
+
+python -u mcts.py --num_nodes 40 --num_games 1000 --max_simulations 5 --rollout_count 10 --game_version=easy --game=mst --quiet > "mcts_results/easy40_5sim_10roll_node.out" 2> "mcts_results/easy40_5sim_10roll_node.err" &
+python -u mcts.py --num_nodes 40 --num_games 1000 --max_simulations 3 --rollout_count 3 --game_version=easy --game=mst --quiet > "mcts_results/easy40_3sim_3roll_node.out" 2> "mcts_results/easy40_3sim_3roll_node.err" &
+
+python -u mcts.py --num_nodes 40 --num_games 1000 --max_simulations 5 --rollout_count 10 --game_version=medium --game=mst_medium --quiet > "mcts_results/medium40_5sim_10roll_node.out" 2> "mcts_results/medium40_5sim_10roll_node.err" &
+python -u mcts.py --num_nodes 40 --num_games 1000 --max_simulations 3 --rollout_count 3 --game_version=medium --game=mst_medium --quiet > "mcts_results/medium40_3sim_3roll_node.out" 2> "mcts_results/medium40_3sim_3roll_node.err" &
+
+python -u mcts.py --num_nodes 50 --num_games 1000 --max_simulations 5 --rollout_count 10 --game_version=easy --game=mst --quiet > "mcts_results/easy50_5sim_10roll_node.out" 2> "mcts_results/easy50_5sim_10roll_node.err" &
+python -u mcts.py --num_nodes 50 --num_games 1000 --max_simulations 3 --rollout_count 3 --game_version=easy --game=mst --quiet > "mcts_results/easy50_3sim_3roll_node.out" 2> "mcts_results/easy50_3sim_3roll_node.err" &
+
+python -u mcts.py --num_nodes 50 --num_games 1000 --max_simulations 5 --rollout_count 10 --game_version=medium --game=mst_medium --quiet > "mcts_results/medium50_5sim_10roll_node.out" 2> "mcts_results/medium50_5sim_10roll_node.err" &
+python -u mcts.py --num_nodes 50 --num_games 1000 --max_simulations 3 --rollout_count 3 --game_version=medium --game=mst_medium --quiet > "mcts_results/medium50_3sim_3roll_node.out" 2> "mcts_results/medium50_3sim_3roll_node.err" &
+
+
+python -u mcts.py --num_nodes 100 --num_games 1000 --max_simulations 5 --rollout_count 10 --game_version=easy --game=mst --quiet > "mcts_results/easy100_5sim_10roll_node.out" 2> "mcts_results/easy100_5sim_10roll_node.err" &
+python -u mcts.py --num_nodes 100 --num_games 1000 --max_simulations 3 --rollout_count 3 --game_version=easy --game=mst --quiet > "mcts_results/easy100_3sim_3roll_node.out" 2> "mcts_results/easy100_3sim_3roll_node.err" &
+
+python -u mcts.py --num_nodes 100 --num_games 1000 --max_simulations 5 --rollout_count 10 --game_version=medium --game=mst_medium --quiet > "mcts_results/medium100_5sim_10roll_node.out" 2> "mcts_results/medium100_5sim_10roll_node.err" &
+python -u mcts.py --num_nodes 100 --num_games 1000 --max_simulations 3 --rollout_count 3 --game_version=medium --game=mst_medium --quiet > "mcts_results/medium100_3sim_3roll_node.out" 2> "mcts_results/medium100_3sim_3roll_node.err" &
